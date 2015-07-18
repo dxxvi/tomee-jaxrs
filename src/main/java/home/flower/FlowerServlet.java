71fa7f38ee0c5229993ac6c1890c0ad81022c044
Thai <dxxvi@yahoo.com>
2015-07-18 05:02:03 -0400

    initial steps for a project to download/upload from openshift

---
+++ src/main/java/home/flower/FlowerServlet.java
@@ -0,0 +1,10 @@
+package home.flower;
+
+import javax.servlet.http.HttpServlet;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class FlowerServlet extends HttpServlet {
+    private static final Logger logger = LogManager.getLogger(FlowerServlet.class);
+}
--- src/main/webapp/index.jsp
+++
@@ -1,8 +0,0 @@
-<!doctype html>
-<html>
-<head></head>
-<body>
-<p><a href="${pageContext.request.contextPath}/resource/word/Me">word resource</a></p>
-<p><a href="${pageContext.request.contextPath}/resource/teacher/You">teacher resource</a></p>
-</body>
-</html>
